fd68927d82881b219931561edf6a82b1bd489a1a
MRSgkce <mursidegokce985@gmail.com>
2024-09-23 23:23:02 +0300

    Temel seviye kişileri tableview ile görüntüleme ve seçilen kişiyi güncelleme için seçilen satırdaki bilgiyi gönderme işlemleri yapıldı. Genel manada temel bir uygulama oldu. Bunu ileride de sık kullanacağız.

---
+++ Kisiler/ui/tableView/kisilerHucre.swift
@@ -0,0 +1,25 @@
+//
+//  kisilerHucre.swift
+//  Kisiler
+//
+//  Created by Mürşide Gökçe on 23.09.2024.
+//
+
+import UIKit
+
+class kisilerHucre: UITableViewCell {
+
+    @IBOutlet weak var labelKisi: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    @IBOutlet weak var labelTel: UILabel!
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Kisiler/ui/views/Anasayfa.swift
+++ Kisiler/ui/views/Anasayfa.swift
@@ -7,24 +7,33 @@
 
 import UIKit
 
-class Anasayfa: UIViewController,UISearchBarDelegate {
+class Anasayfa: UIViewController {
 
     
     @IBOutlet weak var seacrhBar: UISearchBar!
+    @IBOutlet weak var kisilerTableView: UITableView!
+    var kisilerListesi = [Kisiler]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         seacrhBar.delegate = self
+        let k1 = Kisiler(kisi_id:1, kisi_ad:"Mürşide",  kisi_tel: "12345")
+        let k3 = Kisiler(kisi_id: 2, kisi_ad: "mustafa", kisi_tel: "345768")
+        kisilerListesi.append(k1)
+        kisilerListesi.append(k3)
+        kisilerTableView.delegate = self
+        kisilerTableView.dataSource = self
+        //kisilerTableView.reloadData()
         
         
     }
     
-    
-    @IBAction func detay(_ sender: Any) {
-        let kisi = Kisiler(kisi_id:1, kisi_ad: "merve",kisi_tel: "0876534")
-        performSegue(withIdentifier: "toDetay", sender: kisi)
+    override func viewWillAppear(_ animated: Bool) {
+        print("anasayfa göründü")
     }
+    
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "toDetay"{
@@ -37,9 +46,62 @@ class Anasayfa: UIViewController,UISearchBarDelegate {
         }
     }
     
+    
+    
+}
+
+extension Anasayfa: UISearchBarDelegate {//sınıfı genişlettik
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print("kişi arama: \(searchText)")
     }
-    
 }
+
+extension Anasayfa: UITableViewDelegate, UITableViewDataSource{
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return kisilerListesi.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let kisi = kisilerListesi[indexPath.row]
+        let hucre=tableView.dequeueReusableCell(withIdentifier: "kisilerHucre") as! kisilerHucre
+        
+        hucre.labelKisi.text=kisi.kisi_ad
+        hucre.labelTel.text=kisi.kisi_tel
+        
+        return hucre
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let kisi = kisilerListesi[indexPath.row]
+        performSegue(withIdentifier: "toDetay", sender: kisi)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let sil = UIContextualAction(style: .destructive, title: "Sil") { (contextualAction, sourceView, bool) in
+            let kisi = self.kisilerListesi[indexPath.row]
+            let alert = UIAlertController(title: "Kişi Silme", message: "\(kisi.kisi_ad!) Kişi Silinsin mi?", preferredStyle: .alert)
+            
+            let iptal = UIAlertAction(title: "İptal", style: .cancel)
+            alert.addAction(iptal)
+            
+            let sil = UIAlertAction(title: "Sil", style: .destructive) { (action) in
+                print("kişi sil : \(kisi.kisi_id!)")
+                
+            }
+            alert.addAction(sil)
+            self.present(alert,animated: true)
+            
+        }
+        return UISwipeActionsConfiguration(actions: [sil])
+    }
+        
+    }
+        
+        
+        
+    
+    
 
